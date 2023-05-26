bf849590013f30ce2709aef480a191f003c4c490
Pouya Mazji <thepouya.ce@gmail.com>
2023-05-26 21:50:00 +0330

    Features

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -5,8 +5,8 @@ const VerticalFeatures = () => (
   <div dir="rtl">
     <Section>
       <VerticalFeatureRow
-        title="DevOps Services"
-        description="تیم ما تشکیب شدخ"
+        title="DevOps as a Service"
+        description="سرویس پارس دوآپس فرایندی بر مبنای ارتباط و همکاری هرچه بیشتر میان تیم‌های توسعهٔ نرم‌افزار و تیم‌های اجرایی بنا شده است که در طی این فرآیند عملیات توسعهٔ نرم‌افزار و همچنین اِعمال تغییرات زیرساختی به‌ صورت خودکار درمی‌آیند و در کل هدف از چنین فرایندی ایجاد فرهنگی است که در آن تولید، تست و انتشار نرم‌افزار به شیوه‌ای سریع، مداوم و مطمئن انجام شود."
         image="\assets\images\DevopsService.png"
         imageAlt="First feature alt text"
       />
@@ -18,10 +18,23 @@ const VerticalFeatures = () => (
         reverse
       />
       <VerticalFeatureRow
-        title="Your title here"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-        image="/assets/images/feature3.svg"
+        title="Scalibility"
+        description="پارس دوآپس این امکان را میدهد تا بتوانید زیرساخت‌ها و فرآیندهای توسعه خود را در مقیاس مناسب مدیریت و راه‌اندازی کنید. اتوماسیون و ثبات سرویس دهی به شما کمک می‌کند تا سامانه‌های پچیده و در حال تغییر را به صورت کارآمد و با کاهش خطرات احتمالی مدیریت کنید"
+        image="\assets\images\scalibility.png"
         imageAlt="Third feature alt text"
+      />
+      <VerticalFeatureRow
+        title="Customer satisfaction"
+        description="سرویس پارس دوآپس به شما کمک میکند فرآیند توسعه، تست و انتشار نرم‌افزار به صورت خودکار و مداوم انجام شود. این ویژگی باعث می‌شود که زمان تحویل نرم‌افزار به مشتریان کاهش یابد،  کیفیت آن بهبود پیدا کند و خطاهای احتمالی در طول فرآیند به حداقل برسند."
+        image="\assets\images\feature3.svg"
+        imageAlt=" feature alt text"
+        reverse
+      />
+      <VerticalFeatureRow
+        title="Security"
+        description="با استفاده از خدمات ما میتوانید در مارحل و فاز های مختلف از امنیت محصول خود اطمینان حاصب کنید و با  استفاده از اسکن های تخصصی پارس دوآپس از آسیب پذیری های محصول خود مطلع شوید."
+        image="\assets\images\Security.png"
+        imageAlt=" feature alt text"
       />
     </Section>
   </div>
